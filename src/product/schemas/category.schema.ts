c15c2cc5674661c5c5d6f62136ca2b1abdd5cdea
copy-ninja1000 <emmajnr1000@gmail.com>
2024-07-13 23:10:09 +0100

    categories

---
+++ src/product/schemas/category.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument, Types } from 'mongoose';
+
+@Schema({ timestamps: false, collection: 'productcategories' })
+export class ProductCategory extends Document {
+  @Prop({ type: String })
+  name: string;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'productSubCategory' }] })
+  subCategory: string[];
+
+  @Prop({ type: String })
+  id: String;
+
+  @Prop({ type: Number, default: null })
+  totalProducts: number;
+}
+
+export type ProductCategoryDocument = HydratedDocument<ProductCategory>;
+
+export const ProductCategorySchema =
+  SchemaFactory.createForClass(ProductCategory);
