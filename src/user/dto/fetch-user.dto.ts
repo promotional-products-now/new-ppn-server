ffecd0a8f7b8cef1584a61ae0c61856e69aaef08
kenedilichi1 <henryobeta@gmail.com>
2024-07-14 12:04:29 +0100

    implement logic and create endpoint to filter users by the date they were created

--- src/user/dto/fetch-user.dto.ts
+++ src/user/dto/fetch-user.dto.ts
@@ -1,9 +1,46 @@
-import { UserDocument } from '../schemas/user.schema';
+import {
+  IsArray,
+  IsBoolean,
+  IsDateString,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+import { User, UserDocument } from '../schemas/user.schema';
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { PaginationDto } from 'src/commons/dtos/pagination.dto';
 
 export class FindUsers {
-  users: UserDocument[];
+  @ApiProperty({ type: User, isArray: true })
+  @IsArray()
+  @Type(() => User)
+  users: User[];
+
+  @ApiProperty({ example: true, description: 'Is there a previous page?' })
+  @IsBoolean()
   hasPrevious: boolean;
+
+  @ApiProperty({ example: true, description: 'Is there a next page?' })
+  @IsBoolean()
   hasNext: boolean;
+
+  @ApiProperty({ example: 1, description: 'The next page number' })
+  @IsNumber()
   nextPage: number;
+
+  @ApiProperty({ example: 0, description: 'The pervious page number' })
+  @IsNumber()
   prevPage: number;
+}
+
+export class FilterWithCreatedAt extends PaginationDto {
+  @ApiProperty({ required: false, example: '2024-07-20' })
+  @IsDateString()
+  @IsOptional()
+  startDate: string;
+
+  @ApiProperty({ required: false, example: '2024-07-20' })
+  @IsDateString()
+  @IsOptional()
+  endDate: string;
 }
